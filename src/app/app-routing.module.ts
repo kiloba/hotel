228881dbe3cea02af626f975529f2bb16adfa645
kiloba <sebastiannpacheco2003@gmail.com>
2022-05-11 15:55:42 -0500

    se agrega la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Pagina1Component } from './paginas/pagina1/pagina1.component';
+import { Pagina2Component } from './paginas/pagina2/pagina2.component';
+import { Pagina3Component } from './paginas/pagina3/pagina3.component';
+import { Pagina4Component } from './paginas/pagina4/pagina4.component';
+import { Pagina5Component } from './paginas/pagina5/pagina5.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'pagina1', component: Pagina1Component},
+  { path: 'pagina2', component: Pagina2Component},
+  { path: 'pagina3', component: Pagina3Component},
+  { path: 'pagina4', component: Pagina4Component},
+  { path: 'pagina5', component: Pagina5Component}
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { PieComponent } from './componentes/pie/pie.component';
+import { Pagina1Component } from './paginas/pagina1/pagina1.component';
+import { Pagina2Component } from './paginas/pagina2/pagina2.component';
+import { Pagina3Component } from './paginas/pagina3/pagina3.component';
+import { Pagina4Component } from './paginas/pagina4/pagina4.component';
+import { Pagina5Component } from './paginas/pagina5/pagina5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PieComponent,
+    Pagina1Component,
+    Pagina2Component,
+    Pagina3Component,
+    Pagina4Component,
+    Pagina5Component
   ],
   imports: [
     BrowserModule,
